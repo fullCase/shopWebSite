e3f10f102ab6909ed56945cb795b068717a296c5
Łukasz Odolczyk <72561970+fullCase@users.noreply.github.com>
2021-11-04 21:30:34 +0100

    add new file
    add main function file using javascript.

---
+++ function.js
@@ -0,0 +1,4 @@
+const rangeSlider1 = document.getElementById("range1");
+const rangeSlider2 = document.getElementById("range2");
+const rangeSlider1 = document.getElementById("rangeValue1");
+const rangeSlider2 = document.getElementById("rangeValue2");
